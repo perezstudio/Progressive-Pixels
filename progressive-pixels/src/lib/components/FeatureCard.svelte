b12cc55f29f398751559334a17e66e55905ad4a1
Kevin Perez <kp952547@gmail.com>
2023-12-30 12:57:38 -0500

    Refactor FeatureCard and +page.svelte components

--- progressive-pixels/src/lib/components/FeatureCard.svelte
+++ progressive-pixels/src/lib/components/FeatureCard.svelte
@@ -8,8 +8,8 @@
     /** @type {string} */
      export let imageAlt
 </script>
-<div>
-    <img src="{image}" alt="{imageAlt}" />
-    <h3>{title}</h3>
-    <p>{description}</p>
+<div class="flex flex-col content-center p-8">
+    <img src="{image}" alt="{imageAlt}" class="h-20"/>
+    <h3 class="text-slate-950 text-4xl font-bold text-center">{title}</h3>
+    <p class="text-slate-950 font-medium text-center">{description}</p>
 </div>
