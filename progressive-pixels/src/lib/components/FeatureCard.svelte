3c0883db302cfa3c008e54d2044e0dd443fddd8f
Kevin Perez <kp952547@gmail.com>
2023-12-30 12:21:13 -0500

    Add FeatureCard component and update page content

---
+++ progressive-pixels/src/lib/components/FeatureCard.svelte
@@ -0,0 +1,15 @@
+<script>
+    /** @type {string} */
+     export let title
+    /** @type {string} */
+     export let description
+    /** @type {string} */
+     export let image
+    /** @type {string} */
+     export let imageAlt
+</script>
+<div>
+    <img src="{image}" alt="{imageAlt}" />
+    <h3>{title}</h3>
+    <p>{description}</p>
+</div>
--- progressive-pixels/src/routes/+page.svelte
+++ progressive-pixels/src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1 class="text-red-900">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { FeatureCard } from '$lib';
+</script>
+<section>
+    <h1>A design agency tailored for your cause</h1>
+    <p>The Unlimited Design Subscription Service for Political Campaigns, Parties, and Nonprofits.</p>
+    <a href="/contact"><p>Sign Up for Early Access</p></a>
+</section>
+<section>
+    <div>
+        <div>
+            <p>Membership Benefits</p>
+            <h2>Unmatched Perks for Members</h2>
+            <p>In the realm of progressive movements, every message is a stepping stone towards impactful change. Our Creative as a Service (CaaS) subscription model is meticulously crafted to amplify your voice, offering a myriad of benefits tailored for political organizations, campaigns, nonprofits, and entities advocating for positive change.</p>
+            <a href="/contact"><p>See Plans</p></a>
+        </div>
+        <div>
+            <FeatureCard title="Completely Asynchronous" description="Say goodbye to endless meetings while still reaching your creative goals." image="AsynchronousIcon.svg" imageAlt="Async Icon" />
+            <FeatureCard title="Easy to Use Design Portal" description="Effortlessly manage your creative queue with our intuitive portal." image="PortalIcon.svg" imageAlt="Portal Icon"/>
+            <FeatureCard title="Unlimited Team Members" description="Every team member's voice can contribute to driving impactful change." image="TeamMembersIcon.svg" imageAlt="Team Members Icon"/>
+            <FeatureCard title="Fixed Monthly Rate" description="No surprises! Pay the same fixed price each month." image="MonthlyRateIcon.svg" imageAlt="Monthly Rate Icon"/>
+            <FeatureCard title="Lightning Fast Delivery" description="Get your design one at a time in just a few days on average." image="DeliveryIcon.svg" imageAlt="Fast Delivery Icon"/>
+            <FeatureCard title="One of a Kind & Exclusively Yours" description="Each of your designs is made especially for you and is 100% yours." image="UniqueGiftIcon.svg" imageAlt="Unique Design Icon"/>
+        </div>
+    </div>
+</section>
