eb46888fd042124c40d8478a41712155e9ffa3c2
Kevin Perez <kp952547@gmail.com>
2024-01-01 17:48:50 -0500

    Refactor page layout and add "Coming Spring 2024" label

---
+++ progressive-pixels/src/routes/home/+page.svelte
@@ -0,0 +1,66 @@
+<script>
+    import { FeatureCard } from '$lib';
+</script>
+<section class="bg-slate-950 bg-repeat" style="background-image: url(Background-Square.svg)">
+    <div class="container mx-auto flex flex-col gap-12 py-20 px-10 md:pt-36 md:pb-20">
+        <img src="Logo.svg" alt="Political Campaign" class="max-w-64"/>
+        <div class="flex flex-col gap-4 max-w-xl">
+            <h1 class="text-white text-5xl font-bold">A design agency tailored for your cause</h1>
+            <p class="text-white text-xl font-medium">The Unlimited Design Subscription Service for Political Campaigns, Parties, and Nonprofits.</p>
+            <div>
+                <a href="/contact" class="inline-block"><p class="font-medium text-slate-900 p-4 rounded-lg bg-white">Sign Up for Early Access</p></a>
+            </div>
+        </div>
+        <div>
+            <img src="HeroImage.svg" alt="Hero Image" />
+            <img src="HeroImage.svg" alt="Hero Image" />
+        </div>
+    </div>
+</section>
+<section class="px-6 py-20">
+    <div class="container mx-auto flex flex-col items-center gap-8">
+        <div class="flex flex-col items-center max-w-2xl gap-4">
+            <p class="text-center font-bold text-cyan-500 uppercase">Membership Benefits</p>
+            <h2 class="text-center text-4xl text-slate-950 font-bold">Unmatched Perks for Members</h2>
+            <p class="text-center text-slate-950">In the realm of progressive movements, every message is a stepping stone towards impactful change. Our Creative as a Service (CaaS) subscription model is meticulously crafted to amplify your voice, offering a myriad of benefits tailored for political organizations, campaigns, nonprofits, and entities advocating for positive change.</p>
+            <a href="/contact"><p class="bg-slate-900 text-white font-medium px-6 py-4 rounded-lg hover:bg-slate-800">See Plans</p></a>
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <FeatureCard title="Completely Asynchronous" description="Say goodbye to endless meetings while still reaching your creative goals." image="AsynchronousIcon.svg" imageAlt="Async Icon" />
+            <FeatureCard title="Easy to Use Design Portal" description="Effortlessly manage your creative queue with our intuitive portal." image="PortalIcon.svg" imageAlt="Portal Icon"/>
+            <FeatureCard title="Unlimited Team Members" description="Every team member's voice can contribute to driving impactful change." image="TeamMembersIcon.svg" imageAlt="Team Members Icon"/>
+            <FeatureCard title="Fixed Monthly Rate" description="No surprises! Pay the same fixed price each month." image="MonthlyRateIcon.svg" imageAlt="Monthly Rate Icon"/>
+            <FeatureCard title="Lightning Fast Delivery" description="Get your design one at a time in just a few days on average." image="DeliveryIcon.svg" imageAlt="Fast Delivery Icon"/>
+            <FeatureCard title="One of a Kind & Exclusively Yours" description="Each of your designs is made especially for you and is 100% yours." image="UniqueGiftIcon.svg" imageAlt="Unique Design Icon"/>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container mx-auto">
+        <div>
+            <div>
+                <img src="Logo.svg" alt="Political Campaign" />
+                <h2>Design Essentials</h2>
+            </div>
+            <h3>Explore a Spectrum of Creative Services Tailored for Impact!</h3>
+            <p>From striking campaign graphics and engaging social media content to compelling visual identities and informative infographics, we bring your progressive vision to life. Our dedicated team of designers collaborates with you at every step, ensuring each creation is a powerful representation of your mission and values. Let's shape the future with visuals that inspire action, foster unity, and drive change!</p>
+            <div>
+                <div>
+                    <h4>Where Design Meets Brand Consistency</h4>
+                    <p>Our approach intertwines your unique brand story with consistent, innovative design. This synergy creates a compelling visual narrative that resonates with your audience and reinforces your brand's essence.</p>
+                </div>
+                <div>
+                    <h4>Where Design Meets Brand Consistency</h4>
+                    <p>Our approach intertwines your unique brand story with consistent, innovative design. This synergy creates a compelling visual narrative that resonates with your audience and reinforces your brand's essence.</p>
+                </div>
+                <div>
+                    <h4>Where Design Meets Brand Consistency</h4>
+                    <p>Our approach intertwines your unique brand story with consistent, innovative design. This synergy creates a compelling visual narrative that resonates with your audience and reinforces your brand's essence.</p>
+                </div>
+            </div>
+        </div>
+        <div>
+            <img src="DesignEssentials.svg" alt="Design Essentials" />
+        </div>
+    </div>
+</section>
