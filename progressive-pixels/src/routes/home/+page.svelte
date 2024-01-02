5016699d80001869fd4ae1c25a67678c024d7717
Kevin Perez <kp952547@gmail.com>
2024-01-02 13:37:49 -0500

    Update home page layout and add new images

--- progressive-pixels/src/routes/home/+page.svelte
+++ progressive-pixels/src/routes/home/+page.svelte
@@ -1,20 +1,27 @@
 <script>
     import { FeatureCard } from '$lib';
 </script>
-<section class="bg-slate-950 bg-repeat" style="background-image: url(Background-Square.svg)">
-    <div class="container mx-auto flex flex-col gap-12 py-20 px-10 md:pt-36 md:pb-20">
+<section class="bg-slate-950 bg-repeat h-screen overflow-hidden" style="background-image: url(Background-Square.svg)">
+    <div class="container mx-auto flex flex-col gap-12 py-20 px-10">
         <img src="Logo.svg" alt="Political Campaign" class="max-w-64"/>
         <div class="flex flex-col gap-4 max-w-xl">
-            <h1 class="text-white text-5xl font-bold">A design agency tailored for your cause</h1>
+            <div class="inline-flex">
+                <p class="text-sm outline rounded-3xl text-slate-400 px-4 py-1">Coming Spring 2024</p>
+            </div>
+            <h1 class="text-white text-4xl lg:text-5xl font-bold">A design agency tailored for your cause</h1>
             <p class="text-white text-xl font-medium">The Unlimited Design Subscription Service for Political Campaigns, Parties, and Nonprofits.</p>
             <div>
-                <a href="/contact" class="inline-block"><p class="font-medium text-slate-900 p-4 rounded-lg bg-white">Sign Up for Early Access</p></a>
+                <a href="https://airtable.com/appiFXoC3pM5DEqVq/pag9jjvQuZv4WiL5R/form" target=”_blank” class="inline-block"><p class="font-medium text-slate-900 p-4 rounded-lg bg-white">Sign Up for Early Access</p></a>
             </div>
         </div>
-        <div>
-            <img src="HeroImage.svg" alt="Hero Image" />
-            <img src="HeroImage.svg" alt="Hero Image" />
-        </div>
+    </div>
+    <div class="hidden lg:flex flex-row gap-8 px-10 lg:max-h-64 justify-center">
+        <img src="gallery/sevier-county-dems@3x.png" alt="Sevier County Dems Logo" class="w-[26.25rem] h-[15.625rem] rounded-lg" />
+        <img src="gallery/publius-polls@3x.png" alt="Publius Polls Website" class="w-[26.25rem] h-[15.625rem] rounded-lg" />
+        <img src="gallery/hispanic-heritage-month@3x.png" alt="Hispanic Heritage Month Social Posts" class="w-[26.25rem] h-[15.625rem] rounded-lg" />
+        <img src="gallery/black-history-month@3x.png" alt="Black History Month Website" class="w-[26.25rem] h-[15.625rem] rounded-lg" />
+        <img src="gallery/fair-deal-site@3x.png" alt="One Fair Deal Website" class="w-[26.25rem] h-[15.625rem] rounded-lg" />
+        <img src="gallery/floral-pattern@3x.png" alt="Hispanic Heritage Month Social Posts" class="w-[26.25rem] h-[15.625rem] rounded-lg" />
     </div>
 </section>
 <section class="px-6 py-20">
